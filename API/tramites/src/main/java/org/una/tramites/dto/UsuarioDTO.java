2837589d15bfd09664922ab782e49fd159820302
SergioVillr <70040516+SergioVillr@users.noreply.github.com>
2020-09-14 01:08:34 -0600

    Commit2

---
+++ API/tramites/src/main/java/org/una/tramites/dto/UsuarioDTO.java
@@ -0,0 +1,29 @@
+package org.una.tramites.dto;
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author Sergio
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor 
+@ToString
+public class UsuarioDTO {
+ 
+    private Long id; 
+    private String nombreCompleto;   
+    private String cedula; 
+    private boolean estado; 
+    private Date fechaRegistro; 
+    private Date fechaModificacion; 
+    private Long departamentoId; 
+    private boolean esJefe;
+     
+}
+
---
+++ API/tramites/src/main/java/org/una/tramites/repositories/IUsuarioRepository.java
@@ -0,0 +1,26 @@
+package org.una.tramites.repositories;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.una.tramites.entities.Usuario;
+
+/**
+ *
+ * @author Sergio
+ */
+public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Usuario findByCedulaAndPasswordEncriptado(String cedula, String passwordEncriptado);
+
+    public List<Usuario> findByCedulaContaining(String cedula);
+
+    public List<Usuario> findByNombreCompletoContainingIgnoreCase(String nombreCompleto);
+    
+    /*@Query("SELECT u FROM Usuario u LEFT JOIN u.departamento d WHERE u.esJefe=1 AND d.id=:id")
+    public Usuario findJefeByDepartamento(Long id);*/
+       @Query("select u from Usuario u where UPPER(u.nombreCompleto) like CONCAT('%',UPPER(:nombreCompleto),'%')") 
+       public Usuario findNombreCompletoWithLikeSQL(@Param("nombreCompleto")String nombreCompleto);
+
+}
