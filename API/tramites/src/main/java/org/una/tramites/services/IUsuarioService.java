c16b1cbbaf19ec6c81f4fd5ffbfef64d2bf0e64c
SergioVillr <70040516+SergioVillr@users.noreply.github.com>
2020-09-14 13:53:47 -0600

    API_3

---
+++ API/tramites/src/main/java/org/una/tramites/services/IUsuarioService.java
@@ -0,0 +1,31 @@
+package org.una.tramites.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.una.tramites.entities.Usuario;
+
+/**
+ *
+ * @author Sergio
+ */
+public interface IUsuarioService {
+
+    public Optional<List<Usuario>> findAll();
+
+    public Optional<Usuario> findById(Long id);
+
+    public Optional<List<Usuario>> findByCedulaAproximate(String cedula);
+
+    public Optional<List<Usuario>> findByNombreCompletoAproximateIgnoreCase(String nombreCompleto);
+
+    public Usuario create(Usuario usuario);
+
+    public Optional<Usuario> update(Usuario usuario, Long id);
+
+    public void delete(Long id);
+
+    public void deleteAll();
+
+    public Optional<Usuario> login(Usuario usuario); 
+}
+
