2bfb4f8e2463b14147db3d05a37e0b3129b2d661
1001webb <chrissww9@gmail.com>
2020-09-15 00:58:20 -0600

    Commit kk

---
+++ API/tramites/src/main/java/org/una/tramites/entities/Variaciones.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.tramites.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author Heilen
+ */
+@Entity
+@Table(name = "Variaciones")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Variaciones implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "Trámites_Tipos_Id")
+    private TramiteTipo tramiteTipoId;
+
+    @Column(name = "Grupo")
+    private boolean grupo;
+
+    @Column(length = 100, name = "Descripcion")
+    private String descripcion;
+
+    @Column(name = "Estado")
+    private boolean estado;
+
+    @Column(name = "Fecha_Registro", updatable = false)
+    @Setter(AccessLevel.NONE)
+    @Temporal(TemporalType.DATE)
+    private Date fechaRegistro;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "variacionId")
+    private List<Requisito> requisitos = new ArrayList<>();
+
+    private static final long serialVersionUID = 1L;
+
+    @PrePersist
+    public void prePersist() {
+        estado = true;
+        fechaRegistro = new Date();
+    }
+
+}
