5c0f2d71ec8f16e4500bf8899a1dcd630a2ee0c8
SergioVillr <70040516+SergioVillr@users.noreply.github.com>
2020-09-18 18:39:51 -0600

    Update DataLoader.java

--- API/tramites/src/main/java/org/una/tramites/components/DataLoader.java
+++ API/tramites/src/main/java/org/una/tramites/components/DataLoader.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import org.una.tramites.entities.Permiso;
 import org.una.tramites.entities.PermisoOtorgado;
 import org.una.tramites.entities.Usuario;
+import org.una.tramites.loaders.Permisos;
 import org.una.tramites.services.IPermisoOtorgadoService;
 import org.una.tramites.services.IPermisoService;
 import org.una.tramites.services.IUsuarioService;
@@ -25,49 +26,68 @@ public class DataLoader implements ApplicationRunner {
     private String password;
 
     @Autowired
-    private IUsuarioService usuarioService;
+    private IPermisoService permisoService;
 
     @Autowired
-    private IPermisoService permisoService;
+    private IUsuarioService usuarioService;
 
     @Autowired
     private IPermisoOtorgadoService permisoOtorgadoService;
 
     @Override
     public void run(ApplicationArguments args) {
+        if (permisoService.countByEstado(true) <= 0) {
+            createPermisos();
 
+        }
         if (usuarioService.findByCedula(cedula).isEmpty()) {
-
-            Permiso permiso;
-            final String codigo = "Usu01"; 
-            Optional<Permiso> permisoBuscado = permisoService.findByCodigo(codigo);
-
-            if (permisoBuscado.isEmpty()) { 
-                permiso = new Permiso();
-                permiso.setCodigo(codigo);
-                permiso.setDescripcion("Registrar usuario nuevo");
-                permiso = permisoService.create(permiso);
-
-            } else {
-                permiso = permisoBuscado.get();
-            }
-            
-            Usuario usuario = new Usuario();
-            usuario.setNombreCompleto("Sergio Villanueva");
-            usuario.setCedula(cedula);
-            usuario.setPasswordEncriptado(password);
-            usuario = usuarioService.create(usuario);
-
-            PermisoOtorgado permisoOtorgado = new PermisoOtorgado();
-            permisoOtorgado.setPermiso(permiso);
-            permisoOtorgado.setUsuario(usuario);
-            permisoOtorgadoService.create(permisoOtorgado);
-
+            createAdmin(getPermisoCrearUsuario());
             System.out.println("Se agrega el usuario inicial");
+
         } else {
             System.out.println("Se encontro el admin");
+
         }
 
+    }
+
+    private void createPermisos() {
+        for (Permisos permiso : Permisos.values()) {
+            Permiso nuevoPermiso = new Permiso();
+            nuevoPermiso.setCodigo(permiso.getCodigo());
+            nuevoPermiso.setDescripcion(permiso.name());
+            permisoService.create(nuevoPermiso);
+        }
+    }
+
+    private Permiso getPermisoCrearUsuario() {
+
+        Permiso permisoCrearUsuario;
+        final String codigoPermiso = "USU1";
+        Optional<Permiso> permisoBuscado = permisoService.findByCodigo(codigoPermiso);
+
+        if (permisoBuscado.isPresent()) {
+            permisoCrearUsuario = permisoBuscado.get();
+        } else {
+            permisoCrearUsuario = new Permiso();
+            permisoCrearUsuario.setCodigo(codigoPermiso);
+            permisoCrearUsuario.setDescripcion("Crear usuario");
+            permisoCrearUsuario = permisoService.create(permisoCrearUsuario);
+        }
+        return permisoCrearUsuario;
+    }
+
+    private void createAdmin(Permiso permisoCrearUsuario) {
+        Usuario usuario = new Usuario();
+        usuario.setNombreCompleto("Usuario Admin");
+        usuario.setCedula(cedula);
+        usuario.setPasswordEncriptado(password);
+        usuario = usuarioService.create(usuario);
+
+        PermisoOtorgado permisoOtorgado = new PermisoOtorgado();
+        permisoOtorgado.setPermiso(permisoCrearUsuario);
+        permisoOtorgado.setUsuario(usuario);
+        permisoOtorgadoService.create(permisoOtorgado);
     }
 }
 
